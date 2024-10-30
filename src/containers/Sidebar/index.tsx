20f72143dda0b317c528d84b1b9ce2fd728ec726
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-10-30 13:50:36 -0300

    Create Components Titulo, Sidebar,Sobre and Projetos

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import Titulo from "../../components/Titulo"
+
+/* eslint-disable jsx-a11y/alt-text */
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/AlcidesThiago2016.png" />
+    <Titulo>Alcides Thiago</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from "../../components/Titulo";
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre Mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quidem officia,
+       quo repellendus exercitationem in velit alias error asperiores id sit, animi
+        accusamus, quos quisquam cumque fugit earum nemo ad.
+    </p>
+  </section>
+)
+
+export default Sobre
