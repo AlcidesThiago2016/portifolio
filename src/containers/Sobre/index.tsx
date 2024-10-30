8fe29ac2b119bc21ad473368b82e34b5f21bf7e8
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-10-30 16:18:47 -0300

    Create section sobre mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Titulo from "../../components/Titulo";
 import Paragrafo from "../../components/Paragrafo";
+import { GitHubSecao } from "./styles";
 
 const Sobre = () => (
   <section>
@@ -9,6 +11,11 @@ const Sobre = () => (
       officia, quo repellendus exercitationem in velit alias error asperiores id
       sit, animi accusamus, quos quisquam cumque fugit earum nemo ad.
     </Paragrafo>
+    <GitHubSecao>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVFTdcW2W9NofAWpuWmQZOjY8RTyOLJ6_9AQ&s"/>
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=alcidesthiago2016&layout=compact&langs_count=7&theme=dark"/>
+      <img src="https://keyholesoftware.com/wp-content/uploads/Spring-Boot.png"/>
+    </GitHubSecao>
   </section>
 );
 
